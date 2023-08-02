f6094e89dc2262fcb6e24e9b40666333d4b47d7c
mauro4889 <mauro4889@gmail.com>
2023-08-02 13:34:15 -0300

    continuacion consumo de api

--- front/todo-list/src/app/page.js
+++ front/todo-list/src/app/page.js
@@ -1,26 +1,20 @@
-'use client'
-import { useEffect, useState } from "react";
 import { NewTask } from "../../components/NewTask";
 import { Task } from "../../components/Task";
 import { getData } from "../../utils/allTask";
 
 
-export default async function Home() {
-  const [isTasks, setIsTasks] = useState();
-  
-  const tasks = await getData();
 
-  useEffect(()=>{
-    setIsTasks(tasks)
-  },[])
+export default async function Home() {  
+  const tasks = await getData()
   
   return (
     <div>
       <h1>To do List</h1>
       <NewTask/>
       <div>
-        {isTasks.map((task)=><Task task={task}/>)}
+        {tasks.map((task)=><Task task={task} key={task.id}/>)}
       </div>
     </div>
   );
 }
+
--- front/todo-list/utils/allTask.jsx
+++ front/todo-list/utils/allTask.jsx
@@ -1,6 +1,10 @@
-'use client'
+
 export async function getData() {
-    const res = await fetch("http://localhost:8080/api/tasks");
-    const data = await res.json()
-    return data
+    try {
+        const res = await fetch("http://localhost:8080/api/tasks", { cache: 'no-store' });
+        const data = await res.json()
+        return data
+    } catch (error) {
+        return error
+    }
 }
